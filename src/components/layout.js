c4cc0c540f39a989766e449a207747908b9509ba
Poliak, Daniel <daniel.poliak@accenture.com>
2019-02-14 16:19:28 +0100

    implemented reactstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,50 +1,28 @@
-import React from "react"
-import { css } from "@emotion/core"
-import { Link } from "gatsby"
-import styles from './navbar.module.css'
-import { rhythm } from "../utils/typography"
+import React from "react";
+import { css } from "@emotion/core";
+import { Link } from "gatsby";
+import styles from "./navbar.module.css";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap";
 
 export default ({ children }) => (
   <div className={styles.container}>
-  <ul>
-    <Link to={`/`}>
-      <li
-        css={css`
-          margin-bottom: ${rhythm(1)};
-          margin-right: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-      >
-        HOME
-      </li>
-    </Link>
-    <Link to={`/about`}>
-      <li
-        css={css`
-          margin-bottom: ${rhythm(1)};
-          margin-right: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-      >
-        ABOUT
-      </li>
-    </Link>
-    <Link to={`/posts`}>
-      <li
-        css={css`
-          margin-bottom: ${rhythm(1)};
-          margin-right: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-      >
-        BLOG
-      </li>
-    </Link>
-  </ul >
+    <Navbar color="light" light expand="md">
+      <NavbarToggler onClick={() => {}} />
+      <Collapse isOpen={() => {}} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <Link to={`/`}>HOME</Link>
+          </NavItem>
+          <NavItem>
+            <Link to={`/about`}>ABOUT</Link>
+          </NavItem>
+          <NavItem>
+            <Link to={`/posts`}>BLOG</Link>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
     <hr />
     {children}
   </div>
-)
+);
